d255b21aeffa2d4d729b3819dbc27d9f97290f94
vivien <vhenz@college.harvard.edu>
2025-08-15 12:40:13 +0200

    topbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import Hero from '@/components/(app)/landing/Hero';
 import ActionButtons from '@/components/(app)/landing/ActionButtons';
 import FeatureCards from '@/components/(app)/landing/FeatureCards';
 import Footer from '@/components/(app)/landing/Footer';
+import TopBar from '@/components/(app)/landing/TopBar';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
+      <TopBar />
+      <Footer />
     </div>
   );
 }
