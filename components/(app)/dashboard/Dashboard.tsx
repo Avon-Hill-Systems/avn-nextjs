a6fe11104e70a45a0cf3fb66b5eea0ee8653366c
vivien <vhenz@college.harvard.edu>
2025-08-25 09:19:44 +0200

    deploy

--- components/(app)/dashboard/Dashboard.tsx
+++ components/(app)/dashboard/Dashboard.tsx
@@ -13,6 +13,7 @@ export default function Dashboard() {
         </p>
       </div>
 
+
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         <Card className="p-6">
           <h3 className="text-lg font-semibold mb-2">Active Simulations</h3>
